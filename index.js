048bd8d02e26528a21527c53a32dd915bc4b2699
Anastasia Kirianova <tinykiri@Anastasias-MacBook-Air.local>
2024-07-08 16:32:32 -0600

    added PORT changes

--- index.js
+++ index.js
@@ -86,5 +86,6 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
